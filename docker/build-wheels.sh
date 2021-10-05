de604c386396ebaca73775a90bbe6496845e7920
alberttorosyan <32957250+alberttorosyan@users.noreply.github.com>
2021-10-05 13:49:25 +0400

    Added support for python 3.10

--- docker/build-wheels.sh
+++ docker/build-wheels.sh
@@ -91,8 +91,11 @@ echo "build python wheels"
 for python_version in 'cp36-cp36m' 'cp37-cp37m' 'cp38-cp38' 'cp39-cp39' 'cp310-cp310'
 do
   PYTHON_ROOT=/opt/python/${python_version}/
-  # downgrade to pip-18
-  $PYTHON_ROOT/bin/pip install --upgrade pip==18
+  if [ $python_version != "cp310-cp310" ]
+  then
+    # downgrade to pip-18
+    $PYTHON_ROOT/bin/pip install --upgrade pip==18
+  fi
   $PYTHON_ROOT/bin/python setup.py bdist_wheel -d linux_dist
   rm -rf build
 done
