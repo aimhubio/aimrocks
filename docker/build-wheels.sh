489881fb5880666a917b051b903e9c73016d698d
alberttorosyan <32957250+alberttorosyan@users.noreply.github.com>
2021-10-27 22:35:23 +0400

    switch to rocksdb 6.25 for linux builds (#6)
    * Bump rockdb version to 6.25.3
    
    * fail pipeline on script errors
    
    * Build rocksdb with snappy support
    Removed conda-forge distribution of rocksdb (no support of 6.25 version)
    Use compression libraries source tarballs from github repo
    
    * bump aimrocks version

--- docker/build-wheels.sh
+++ docker/build-wheels.sh
@@ -15,16 +15,16 @@ fi
 echo "Installing 3rd party libraries."
 
 # zlib
-wget http://www.zlib.net/zlib-1.2.11.tar.gz
-tar -xzf zlib-1.2.11.tar.gz
+cp aimrocks/deps/zlib/zlib-1.2.11.tar.gz .
+tar zxvf zlib-1.2.11.tar.gz
 cd zlib-1.2.11/
 ./configure && make CFLAGS='-fPIC' CXXFLAGS='-fPIC' && make install
 cd ..
 rm -rf zlib-1.2.11/ zlib-1.2.11.tar.gz
 
 #bzip2
-wget https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz
-tar -xzf bzip2-1.0.8.tar.gz
+cp aimrocks/deps/bzip2/bzip2-1.0.8.tar.gz .
+tar zxvf bzip2-1.0.8.tar.gz
 cd bzip2-1.0.8/
 make CFLAGS='-fPIC' CXXFLAGS='-fPIC' && make install
 cd ..
@@ -55,33 +55,22 @@ tar zxvf snappy-1.1.8.tar.gz
 cd snappy-1.1.8
 mkdir build
 cd build
-cmake CFLAGS='-fPIC' CXXFLAGS='fPIC' -DCMAKE_POSITION_INDEPENDENT_CODE=ON .. && make
+cmake CFLAGS='-fPIC' CXXFLAGS='fPIC' -DCMAKE_POSITION_INDEPENDENT_CODE=ON .. && make && make install
 cp libsnappy.a /usr/local/lib/
 cd ../..
 rm -rf snappy-1.1.8 snappy-1.1.8.tar.gz
 
 #rocksdb static lib
-if ls /etc/redhat-release
-then
-  wget https://github.com/facebook/rocksdb/archive/6.13.fb.tar.gz
-  mv 6.13.fb.tar.gz rocksdb-6.13.fb.tar.gz
-  tar zxvf rocksdb-6.13.fb.tar.gz
-  cd rocksdb-6.13.fb
-  PORTABLE=1 DEBUG_LEVEL=0 make -j2 shared_lib
-  PORTABLE=1 DEBUG_LEVEL=0 make static_lib
-  strip --strip-debug librocksdb.a
-  PORTABLE=1 DEBUG_LEVEL=0 make install-static
-  cd ..
-  rm -rf rocksdb-6.13.fb rocksdb-6.13.fb.tar.gz
-else
-  mkdir rocksdb && cd rocksdb
-  wget https://anaconda.org/conda-forge/rocksdb/6.13.3/download/linux-64/rocksdb-6.13.3-hda8cf21_2.tar.bz2
-  tar -xf rocksdb-6.13.3-hda8cf21_2.tar.bz2
-  cp lib/librocksdb.a /usr/local/lib/
-  cp -r include/rocksdb/ /usr/local/include/
-  cd ..
-  rm -rf rocksdb
-fi
+wget https://github.com/facebook/rocksdb/archive/6.25.fb.tar.gz
+mv 6.25.fb.tar.gz rocksdb-6.25.fb.tar.gz
+tar zxvf rocksdb-6.25.fb.tar.gz
+cd rocksdb-6.25.fb
+EXTRA_CFLAGS="-DSNAPPY -I /usr/local/include/" EXTRA_CXXFLAGS="-DSNAPPY -I /usr/local/include/" PORTABLE=1 DEBUG_LEVEL=0 make -j2 shared_lib
+EXTRA_CFLAGS="-DSNAPPY -I /usr/local/include/" EXTRA_CXXFLAGS="-DSNAPPY -I /usr/local/include/" PORTABLE=1 DEBUG_LEVEL=0 make static_lib
+strip --strip-debug librocksdb.a
+PORTABLE=1 DEBUG_LEVEL=0 make install-static
+cd ..
+rm -rf rocksdb-6.25.fb rocksdb-6.25.fb.tar.gz
 
 echo "3rd party libraries install. SUCCESS"
 
