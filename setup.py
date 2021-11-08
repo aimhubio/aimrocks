1848fc58e7eecd7bf18388d4c79eae7aba270be3
mihran113 <vanyanmihran@gmail.com>
2021-11-08 17:53:57 +0400

    Add column family handler and flush options arguments for `flush` method (bumped aimrocks to 0.0.11)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ third_party_libs = [os.path.join(third_party_install_dir, 'lib', 'lib{}.a'.forma
 
 setup(
     name="aimrocks",
-    version='0.0.10',
+    version='0.0.11',
     description='RocksDB wrapper implemented in Cython.',
     setup_requires=['setuptools>=25', 'Cython==3.0.0a9'],
     packages=find_packages('./src'),
