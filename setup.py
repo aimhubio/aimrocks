a045007f611f0c35820d3c788a18b5b747759756
Karen Hambardzumyan <mahnerak@gmail.com>
2022-03-22 02:09:00 +0400

    bump version

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ exts = [
 
 setup(
     name="aimrocks",
-    version='0.1.2',
+    version='0.1.3a1',
     description='RocksDB wrapper implemented in Cython.',
     setup_requires=['setuptools>=25', 'Cython==3.0.0a9'],
     packages=find_packages('./src'),
