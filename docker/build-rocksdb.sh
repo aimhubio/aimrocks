5f8f7e57083aafd5b49ad0293b3fc63c4e28a00d
Karen Hambardzumyan <mahnerak@gmail.com>
2022-03-23 03:22:37 +0400

    Cleanup

--- docker/build-rocksdb.sh
+++ docker/build-rocksdb.sh
@@ -20,7 +20,7 @@ fi
 PORTABLE=1 make shared_lib EXTRA_CXXFLAGS="-fPIC" EXTRA_CFLAGS="-fPIC" USE_RTTI=0 DEBUG_LEVEL=0 -j4
 strip --strip-debug librocksdb.so
 PORTABLE=1 make PREFIX="../" DESTDIR="./" DEBUG_LEVEL=0 install-shared
-cp -L librocksdb.so ../lib/librocksdb.so
+cp librocksdb.so.6.29.3 ../lib/librocksdb.so
 rm ../lib/librocksdb.so.*
 cd ..
 rm -rf rocksdb-6.29.fb rocksdb-6.29.fb.tar.gz
