9531694d189a58d77580054d84597cf4f6b1183a
Karen Hambardzumyan <mahnerak@gmail.com>
2022-03-22 21:27:27 +0400

    Use newer rocksdb

--- docker/build-rocksdb.sh
+++ docker/build-rocksdb.sh
@@ -9,9 +9,9 @@ then
   fi
 fi
 
-curl -L https://github.com/facebook/rocksdb/archive/6.28.fb.tar.gz -o rocksdb-6.28.fb.tar.gz
-tar zxvf rocksdb-6.28.fb.tar.gz
-cd rocksdb-6.28.fb
+curl -L https://github.com/facebook/rocksdb/archive/6.29.fb.tar.gz -o rocksdb-6.29.fb.tar.gz
+tar zxvf rocksdb-6.29.fb.tar.gz
+cd rocksdb-6.29.fb
 if [[ $platform == centos_5 ]]
 then
   cp /opt/aimrocks_deps/rocksdb_sched.patch .
@@ -19,7 +19,7 @@ then
 fi
 PORTABLE=1 make shared_lib EXTRA_CXXFLAGS="-fPIC" EXTRA_CFLAGS="-fPIC" USE_RTTI=0 DEBUG_LEVEL=0 -j4
 PORTABLE=1 make PREFIX="../" DESTDIR="./" DEBUG_LEVEL=0 install-shared
-strip --strip-debug librocksdb.so.6.28.2
-cp librocksdb.so.6.28.2 ../lib/librocksdb.so
+strip --strip-debug librocksdb.so.6.29.3
+cp librocksdb.so.6.29.2 ../lib/librocksdb.so
 cd ..
-rm -rf rocksdb-6.28.fb rocksdb-6.28.fb.tar.gz
+rm -rf rocksdb-6.29.fb rocksdb-6.29.fb.tar.gz
