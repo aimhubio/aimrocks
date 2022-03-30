f21b97a0b6dcef321be1ce6727e10c33e514754a
Karen Hambardzumyan <mahnerak@gmail.com>
2022-03-30 13:58:46 +0400

    Setup auditwheel

--- docker/audit-wheels.sh
+++ docker/audit-wheels.sh
@@ -6,23 +6,23 @@ export AIM_DEP_DIR=/opt/aimrocks_deps
 # These libraries are manually handled
 LIBS_BUNDLED=`ls ${AIM_DEP_DIR}/lib/ \
 | grep .so \
-| sed -r 's/^lib(.*?).so.*?$/\1/g' \
+| sed -r 's/^(lib.*?\.so)\.*?$/\1/g' \
 | uniq \
 | paste -s -d','`
 LD_LIBRARY_PATH=$AIM_DEP_DIR/lib:$LD_LIBRARY_PATH
-INTERNAL_PYTHON=/opt/_internal/tools/bin/python
+
+INTERNAL_PYTHON=/opt/python/cp38-cp38/bin/python
 
 cd /opt/aimrocks
 
 ls -lhatr dist
 
 $INTERNAL_PYTHON -m pip install --upgrade pip
-$INTERNAL_PYTHON -m pip uninstall -y auditwheel
 $INTERNAL_PYTHON -m pip install git+https://github.com/aimhubio/auditwheel.git@include-exclude
 
 for whl in dist/*.whl
 do
-  auditwheel repair ${whl} \
+  $INTERNAL_PYTHON -m auditwheel repair ${whl} \
   --exclude $LIBS_BUNDLED \
   --wheel-dir manylinux_dist
 done
