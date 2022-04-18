febb525dea074d3b9ea371938a1e4698b0d2a339
Karen Hambardzumyan <mahnerak@gmail.com>
2022-04-18 16:47:42 +0400

    Remove unnecessary libs

--- docker/build-snappy.sh
+++ docker/build-snappy.sh
@@ -7,7 +7,5 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX="../.." CFLAGS="-fPIC" CXXFLAGS="fPIC" -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
 make
 make install
-cp libsnappy.so.1 ../../lib/libsnappy.so.1
-ln -s libsnappy.so.1 ../../lib/libsnappy.so
 cd ../..
 rm -rf snappy-1.1.8 snappy-1.1.8.tar.gz
--- docker/build-zstd.sh
+++ docker/build-zstd.sh
@@ -4,6 +4,5 @@ tar zxvf zstd-1.1.3.tar.gz
 cd zstd-1.1.3
 make CFLAGS="-fPIC" CXXFLAGS="-fPIC"
 make install PREFIX=$PWD/..
-cp lib/libzstd.so.1.1.3 ../lib/libzstd.so
 cd ..
 rm -rf zstd-1.1.3 zstd-1.1.3.tar.gz
